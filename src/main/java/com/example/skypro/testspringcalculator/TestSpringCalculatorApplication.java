5eddbdc9cdaa5a5e624cf1a84808652b016309a4
vadim <vadimpoldolin19@gmail.com>
2023-07-10 00:40:39 +0200

    First commit

---
+++ src/main/java/com/example/skypro/testspringcalculator/TestSpringCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.skypro.testspringcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSpringCalculatorApplication.class, args);
+    }
+
+}
